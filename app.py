60e002e796c0b4c1e3bda91f58932f551a348e14
tbahrit.m <m.tbahriti@esi-sba.dz>
2025-11-26 12:27:05 +0100

    first commit

--- app.py
+++ app.py
@@ -1541,9 +1541,9 @@ def main():
     st.sidebar.markdown("---")
     st.sidebar.markdown("### 👥 Équipe")
     st.sidebar.markdown("""
-    - Membre 1
-    - Membre 2  
-    - Membre 3
+    - TBAHRITI Mohammed
+    - YAHIA OUAHMED Yanis  
+    - MOULAI Youcef
     """)
     
     st.sidebar.markdown("---")
